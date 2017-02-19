48f50629f048047ea31a8eeb2e1d13fb758206b4
igor-karpukhin <LinkKellerman@gmail.com>
2017-02-20 00:36:28 +0300

    Added method to search words

--- app/main.py
+++ app/main.py
@@ -1,30 +1,68 @@
 #!/usr/bin/python
-from flask import Flask
+from flask import Flask, request
 import json
-
+import optparse
+from services.redisHelper import RedisHelper
 
 app = Flask(__name__)
+
+global redis_host
+global redis_port
 
 
 @app.route("/documents", methods=["GET"])
 def documents_get():
-    return json.dumps({"Document_GET": "get"})
+    rds = RedisHelper(redis_host, redis_port)
+    return json.dumps({"documentsCount": rds.get_texts_count()})
 
 
-@app.route("/documents/<int:index>", methods=["POST"])
+@app.route("/document/<int:index>", methods=["POST"])
 def documents_post(index):
-    return json.dumps({"Document_POST": index})
+    rds = RedisHelper(redis_host, redis_port)
+    rds.append_text(index, request.data)
+    return json.dumps({"documentIndex": index})
 
 
-@app.route("/documents/<int:index>", methods=["GET"])
-def documents_search(index):
-    return json.dumps({"Document_SEARCH": index})
+@app.route("/document/<int:index>", methods=["GET"])
+def documents_get_by_index(index):
+    rds = RedisHelper(redis_host, redis_port)
+    return json.dumps({"documentData": rds.get_document_by_index(index)})
 
 
-@app.route("/documents/<int:index>", methods=["DELETE"])
+@app.route("/document/<int:index>", methods=["DELETE"])
 def documents_delete(index):
-    return json.dumps({"Documents_DELETE": index})
+    return json.dumps({"documentsDeleted": index})
+
+
+@app.route("/document", methods=["GET"])
+def document_search():
+    q_word = request.args.get("q")
+    if not q_word:
+        return json.dumps({"error": "Request has no 'q' parameter"})
+
+    rds = RedisHelper(redis_host, redis_port)
+    return json.dumps({"documentsIndexes": rds.lookup_word(q_word)})
 
 
 if __name__ == "__main__":
-    app.run()
+    global redis_host
+    global redis_port
+
+    opt_parser = optparse.OptionParser()
+    opt_parser.add_option("--addr", action="store", dest="addr",
+                          default="0.0.0.0", help="IP address to listen")
+    opt_parser.add_option("--port", action="store", dest="port",
+                          default=8080, help="port to listen")
+    opt_parser.add_option("--rhost", action="store", dest="rhost",
+                          default="127.0.0.1", help="Redis host")
+    opt_parser.add_option("--rport", action="store", dest="rport",
+                          default=7760, help="Redis port")
+    args, vals = opt_parser.parse_args()
+
+    redis_host = args.rhost
+    redis_port = args.rport
+
+    print "Using addr: {0}:{1}".format(args.addr, args.port)
+    print "Redis connection: {0}:{1}".format(redis_host, redis_port)
+    app.run(args.addr, args.port)
+
--- app/services/redisHelper.py
+++ app/services/redisHelper.py
@@ -7,5 +7,30 @@ class RedisHelper(object):
         self.port = port
         self.connection = redis.StrictRedis(self.host, self.port)
 
-    def append_word(self, word):
-        pass
+    def append_text(self, index, text):
+        words = text.split(' ')
+        self._set_text_index(index, text)
+        for w in words:
+            self._append_word(w, index)
+
+    def _set_text_index(self, index, text):
+        self.connection.set("text:{0}".format(index), text)
+
+    def _append_word(self, word, text_index):
+        self.connection.zincrby("word:{0}".format(word), text_index, 1)
+
+    def get_texts_count(self):
+        return len(self.connection.keys("text:*"))
+
+    def get_document_by_index(self, index):
+        return self.connection.get("text:{0}".format(index))
+
+    def lookup_word(self, word):
+        print "WORD", word
+        r = self.connection.zunionstore("search:results",
+                                        ["word:{0}".format(word)])
+        print "ZSTR", r
+        results = self.connection.zrevrange("search:results",
+                                            0, -1, True, int)
+        print "RSLT", results
+        return results
